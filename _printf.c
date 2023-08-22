0d32d79a360f514346f297693fe3a1126741482f
somaftees <somaftees@gmail.com>
2023-08-22 14:22:23 -0700

    done

--- _printf.c
+++ _printf.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
+ * _printf - A function that selects the correct function to print
+ * @format: Constant pointer
+ * Return: Length of the string
  */
 int _printf(const char * const format, ...)
 {
